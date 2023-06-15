d2fb29de2e505bc07dbcba04c44e96511c4f96e9
Cverkade <verkadecornelis@gmail.com>
2023-06-15 10:41:51 +0200

    Add Roman to Arabic converter

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export const numeralConvert = (num: number): string => {
+export const arabicToRoman = (num: number): string => {
     
     const importantValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
     let result: string = '';
@@ -6,6 +6,19 @@ export const numeralConvert = (num: number): string => {
         while (num >= digit) {
             result += convertDigit(digit);
             num -= digit;
+        }   
+    }
+    return result;
+}
+
+export const romanToArabic = (romanNumeral: string): number => {
+    
+    const importantValues = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
+    let result: number = 0;
+    for (const numeral of importantValues) {
+        while (romanNumeral.endsWith(numeral)) {
+            result += convertNumeral(numeral);
+            romanNumeral = romanNumeral.substring(0,romanNumeral.lastIndexOf(numeral));
         }   
     }
     return result;
@@ -28,5 +41,24 @@ const convertDigit = (digit: number) : string => {
         case 1000: return "M";
     }
     return 'This didn\'t work';
+}
+
+const convertNumeral = (numeral: string) : number => {
+    switch (numeral) {
+        case "I": return 1;
+        case "IV": return 4;
+        case "V": return 5;
+        case "IX": return 9;
+        case "X": return 10;
+        case "XL": return 40;
+        case "L": return 50;
+        case "XC": return 90;
+        case "C": return 100;
+        case "CD": return 400;
+        case "D": return 500;
+        case "CM": return 900;
+        case "M": return 1000;
+    }
+    return 0;
 }
 
